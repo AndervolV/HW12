5dfbc09bf1d558f373fa4163b558a7c332b6bea4
user <andervol2015@gmail.com>
2025-03-02 14:23:25 +0300

    HW12

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package pro.sky;
+public class Main {
+    public static void main(String[] args) {
+        String nameJohn = "John";
+        int ageJohn = 13;
+
+        String nameSarah = "Sarah";
+        int ageSarah = 30;
+
+        String[] names = {"John", "Sarah"};
+        int[] ages = {13, 30};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Имя - " + names[i] + " Возраст - " + ages[i]);
+        }
+    }
+}
